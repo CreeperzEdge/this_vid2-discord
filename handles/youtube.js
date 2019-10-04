c28adc642a086c18aa870b707e5af7bd2b4654ad
CreeperzEdge <CreeperzEdge@users.noreply.github.com>
2019-10-04 17:37:25 +1000

    escape characters

--- handles/youtube.js
+++ handles/youtube.js
@@ -4,7 +4,7 @@ module.exports = async function (url) {
     return new Promise((resolve, reject) => {
         ytdl.getBasicInfo(url, (err, info) => {
             if (err) reject(err);
-            resolve(info.author.name);
+            resolve(info.author.name.replace(/[\\$'"]/g, "\$&"));
         });
     })
 };
